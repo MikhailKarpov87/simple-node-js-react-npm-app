eafab8cda27742f537e1c16af52d39e0ae7a6708
Mikhail Karpov <mihanikk@gmail.com>
2019-08-10 17:00:40 +0300

    build folder -> gitignore, new test text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className='App-title'>Remote Test</h1>
         </header>
         <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. test2
         </p>
       </div>
     );
